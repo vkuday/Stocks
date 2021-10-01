df19cf6c02fbd92f4e041e9416f974c9dbc0bba5
vkuday <volkuday@hotmail.com>
2021-10-01 13:30:03 -0700

    Set app icon & launch image

---
+++ Stocks/Managers/HapticsManager.swift
@@ -0,0 +1,25 @@
+//
+//  HapticsManager.swift
+//  Stocks
+//
+//  Created by yupana on 10/1/21.
+//
+
+import Foundation
+import UIKit
+
+final class HapticsManager {
+    
+    static let shared = HapticsManager()
+    
+    private init() {}
+    
+    // MARK: - Public
+    
+    public func vibrateForSelection() {
+        // Vibrate lightly for a selection tap interaction
+    }
+    
+    // Vibrate for type
+    
+}
