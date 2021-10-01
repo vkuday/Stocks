703eb9b3fd58387d1a418a5e5f272e9a02e00186
vkuday <volkuday@hotmail.com>
2021-10-01 11:41:40 -0700

    Removed storyboard

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -11,9 +11,8 @@ class WatchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
 
 }
 
